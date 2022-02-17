d0f10b14e17b1e0dbfa25ef96fea088229a208a5
Ben Daniels <benjamindaniels21@gmail.com>
2022-02-17 11:25:54 -0500

    fixed font awesome

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { regular } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
-
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 function Header() {
   return (
     <div className="header">
       <h1>Travel Journal</h1>
-      <FontAwesomeIcon icon={regular("coffee")} />
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
