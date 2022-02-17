8b24264b0e9d178658f79ae82cd09788cb2006a1
Ben Daniels <benjamindaniels21@gmail.com>
2022-02-17 18:34:59 -0500

    experience added and styled

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Experience />
+      <Experience city="JAPAN" />
     </div>
   );
 }
--- src/Experience.js
+++ src/Experience.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+// import japan from "./japan.jpeg";
 
-function Experience() {
-  return <div className="experience">Experience</div>;
+function Experience(props) {
+  console.log(props);
+  return (
+    <div className="experience">
+      <img className="experience--image" src="japan.jpeg" alt="japan" />
+      <div className="experience--info">
+        <h3>{props.city}</h3>
+        <a href="https://google.com">
+          <p>View on Google Maps</p>
+        </a>
+        <h1 className="destination">Mount Fuji</h1>
+        <p className="travel-dates">12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="description">
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+          (12,380 feet). Mount Fuji is the single most popular tourist site in
+          Japan, for both Japanese and foreign tourists.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Experience;
